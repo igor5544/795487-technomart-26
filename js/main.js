e0704c6542c5fc3076c61b9209fa4df78e78f0d5
igor5544 <igor5544@bk.ru>
2019-10-04 04:42:33 +0500

    Start JS

---
+++ js/main.js
@@ -0,0 +1,109 @@
+let modalOverlay = document.querySelector('.overlay');
+let btnFeedback = document.querySelector('.header__links-order');
+let modalFeedback = document.querySelector('.feedback');
+let closeFeedback = modalFeedback.querySelector('.modal__close');
+let formFeedback = modalFeedback.querySelector('.feedback__form');
+let formFeedback__name = formFeedback.querySelector('#user-name');
+let formFeedback__email = formFeedback.querySelector('#user-email');
+let formFeedback__massage = formFeedback.querySelector('#user-message');
+
+let isStorageSupport = true;
+let storageLogin;
+let storageEmail;
+
+try {
+    storageLogin = localStorage.getItem('UserName');
+} catch (err) {
+    isStorageSupport = false;
+}
+
+try {
+    storageEmail = localStorage.getItem('email');
+} catch (err) {
+    isStorageSupport = false;
+}
+
+btnFeedback.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalFeedback.classList.add('modal-show');
+    modalOverlay.classList.add('overlay-show');
+    if (storageLogin) {
+        formFeedback__name.value = storageLogin;
+        formFeedback__email.focus();
+    }
+    if (storageEmail) {
+        formFeedback__email.value = storageEmail;
+        formFeedback__massage.focus();
+    }
+    if (!storageLogin) {
+        formFeedback__name.focus();
+    }
+});
+
+closeFeedback.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalFeedback.classList.remove('modal-show');
+    modalFeedback.classList.remove('modal-error');
+    modalOverlay.classList.remove('overlay-show');
+});
+
+formFeedback.addEventListener('submit', function (evt) {
+    if (!formFeedback__name.value || !formFeedback__email.value || !formFeedback__massage.value) {
+        evt.preventDefault();
+        modalFeedback.classList.remove('modal-error');
+        modalFeedback.offsetWidth = modalFeedback.offsetWidth;
+        modalFeedback.classList.add('modal-error');
+    } else if (isStorageSupport) {
+        localStorage.setItem('UserName', formFeedback__name.value);
+        localStorage.setItem('email', formFeedback__email.value);
+    }
+});
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode == 27) {
+        evt.preventDefault();
+        if (modalFeedback.classList.contains('modal-show')) {
+            modalFeedback.classList.remove('modal-show');
+            modalFeedback.classList.remove('modal-error');
+            modalOverlay.classList.remove('overlay-show');
+        }
+    }
+});
+
+modalOverlay.addEventListener('click', function () {
+    modalFeedback.classList.remove('modal-show');
+    modalFeedback.classList.remove('modal-error');
+    modalAdding.classList.remove('modal-show');
+    modalOverlay.classList.remove('overlay-show');
+});
+
+
+let btnAddingSet = document.querySelectorAll('.price__buy');
+let modalAdding = document.querySelector('.adding');
+let closeAddink = modalAdding.querySelector('.modal__close');
+
+
+for (i = 0; i < btnAddingSet.length; i++) {
+    btnAdding = btnAddingSet[i];
+    btnAdding.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        modalAdding.classList.add('modal-show');
+        modalOverlay.classList.add('overlay-show');
+    });
+}
+
+closeAddink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalAdding.classList.remove('modal-show');
+    modalOverlay.classList.remove('overlay-show')
+});
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode == 27) {
+        evt.preventDefault();
+        if (modalAdding.classList.contains('modal-show')) {
+            modalAdding.classList.remove('modal-show');
+            modalOverlay.classList.remove('overlay-show');
+        }
+    }
+});
